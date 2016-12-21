0dc18fe6ac7ec09d0312a9874c54f4f642909f1e
Carlos Rodrigues <carlos.all.mail@gmail.com>
2016-12-21 01:22:01 -0700

    Added some colour to the log message

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const color = require('chalk')
 
 const PORT = 3000
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+  console.log(color.green(`Listening on port ${PORT}`))
 })
